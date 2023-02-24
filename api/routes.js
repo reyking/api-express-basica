8af477879463b6d0aef7de167f8b68da9abf2da8
Hans Raddatz Garcia <hans@pruebas.hansraddatz.com>
2023-02-24 20:31:25 -0300

    nombre y funcionalidad de varias

--- api/routes.js
+++ api/routes.js
@@ -2,10 +2,10 @@ const archivos = require('./controlers/archivos');
 const mensaje = require('./controlers/mensajes')
 
 module.exports = function routes(app){
-    app.get('/', (req, res) => {
-        res.status(200).json("hola").end();
+    app.get('/v1/mensaje/:mensaje', mensaje);
+    app.get('/v1/archivo', archivos);
+    app.get('/v1*', (req, res) => {
+        res.status(200).json("hola, este metodo no esta implementado").end();
         console.log(req);
     })
-    app.get('/mensaje/:mensaje', mensaje);
-    app.get('/archivo', archivos);
 }
--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const path = require('path')
 routes(app)
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.listen(3000)
+app.listen(5555)
 
 console.log("iniciando")
